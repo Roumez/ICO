d4a10d414498266e24efce62f182f74dd2a6803e
Roumez <remi.roumezy@hotmail.fr>
2021-06-02 09:40:41 +0200

    first commit

---
+++ Contract/FirstIco.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+
+contract FirstIco is IERC20 {
+    
+    // library usage
+
+    // state variables
+
+    // events
+
+    // constructor
+  
+
+    // modifiers
+
+    // functions
+    
+}
---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-solhint')
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.4',
+}
+
